d0d2cfd8bdae5804ff02fa9d2fe99146788cfd16
Suraj Virkar <87970303+surajvirkar6@users.noreply.github.com>
2022-10-05 10:35:01 +0530

    commit

--- searching/linear.js
+++ searching/linear.js
@@ -8,9 +8,22 @@ let array1 = [12, 34, 56, 32, 89, 49];
 const target1 = 89;
 
 let result1 = search1(array1, target1);
+let result2 = search2(array1, target1);
 
 function search1(array, target) {
   array.map((item, index) => {
-    item == target ? index : -1;
+    if (item == target) {
+      return index;
+    }
   });
+  return -1;
+}
+
+function search2(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (target == array[i]) {
+      return i;
+    }
+  }
+  return -1;
 }
