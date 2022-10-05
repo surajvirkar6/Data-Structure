bd5687bd47529f9a6889611ed1c48e768b859a0d
Suraj Virkar <87970303+surajvirkar6@users.noreply.github.com>
2022-10-05 10:22:23 +0530

    linear 1

--- searching/linear.js
+++ searching/linear.js
@@ -1,1 +1,18 @@
-console.log("here");
+/*
+Time Complexity 
+Best case:- O(1) => element found at start
+Average and worst case:- O(n) => element found at last
+*/
+
+let array1 = [12, 34, 56, 32, 89, 49];
+const target1 = 89;
+
+let result1 = search1(array1, target1);
+
+function search1(array, target) {
+  array.map((item, index) => {
+    if (item == target) {
+      return index;
+    }
+  });
+}
